16c6c3fabd6fa3d9ca8e1c5c3bad9a71dfaa1a5d
Nichlos Ho <hon1@seattleu.edu>
2024-04-24 23:15:07 -0700

    created ui routes and files

--- src/pages/app.routes.ts
+++ src/pages/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { LandingComponent } from './landing/landing.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'products/details', component: ProductDetailsComponent },
+    { path: 'checkout', component: CheckoutComponent },
+    { path: '**', component: NotFoundComponent },
+];
