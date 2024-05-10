86746782de9cfca8c1c6ae6cd11580eb5c4b3374
nichlos.ho <hon1@seattleu.edu>
2024-05-09 18:59:57 -0700

    add environments

---
+++ environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    baseUrl: 'http://localhost:',
+    mongoDBConnection:'mongodb+srv://newuser:jmw2LNYyIMv7gCBR@ecolens.dkykdv2.mongodb.net/Ecolens',
+    backendPort: '3000',
+
+};
