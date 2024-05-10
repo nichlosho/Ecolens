c10cfeaac04505884566789f393aea1354eafb9b
Nichlos Ho <hon1@seattleu.edu>
2024-05-10 11:52:19 -0700

    add dist path

--- server.ts
+++ server.ts
@@ -1,15 +1,25 @@
-import { environment } from 'environments/environment';
+import dotenv from 'dotenv';
+import express from 'express';
+import path from 'path';
 import { App } from 'src/App';
 
+dotenv.config();
+
 try {
-    new App(environment.mongoDBConnection).expressApp.listen(
-        environment.backendPort
-    );
+    const __dirname = path.resolve();
+    const app = new App(process.env.MONGODB_CONNECTION_URL);
+    app.expressApp.listen(process.env.BACKEND_PORT);
     console.log(
         '------------- Server is listing on port:' +
-            environment.backendPort +
+            process.env.BACKEND_PORT +
             ' -----------------------------'
     );
+
+    const distPath = path.join(__dirname, './dist/Ecolens');
+    app.expressApp.use(express.static(distPath));
+    app.expressApp.get('/*', (req, res) => {
+        res.sendFile('index.html', { root: distPath });
+    });
 } catch (error) {
     console.error(
         '🛑 ------ !!!!!!!! Database connection failed',
