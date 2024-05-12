a2163390eb7e41ad7c8814214830e34ea0178a25
Nichlos Ho <hon1@seattleu.edu>
2024-05-11 17:31:19 -0700

    add test code

---
+++ src/tests/product.service.test.js
@@ -0,0 +1,45 @@
+import { expect, use } from "chai";
+import chaiHttp from "chai-http";
+const chai = use(chaiHttp);
+
+chai.request();
+
+describe("Test Get All Products API call result", function () {
+    var requestResult;
+    var response;
+
+    before(function (done) {
+        chai.request("http://localhost:3000")
+            .get("/products")
+            .end(function (err, res) {
+                requestResult = res.body;
+                response = res;
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+    it("Should return an array object with more than 2 objects", function () {
+        expect(response).to.have.status(200);
+        expect(response.body).to.have.length.above(0);
+        expect(response).to.have.headers;
+    });
+
+    it("The first entry in the array has known properties", function () {
+        expect(requestResult[0]).to.include.keys("name");
+        expect(requestResult[0]).to.have.property("_id");
+        expect(response.body).to.not.be.a.string;
+    });
+
+    it("The elements in the array have the expected properties", function () {
+        expect(response.body).to.have.length.above(0);
+        expect(response.body).to.satisfy(function (body) {
+            for (var i = 0; i < body.length; i++) {
+                expect(body[i]).to.have.property("name");
+                expect(body[i]).to.have.property("description");
+            }
+            return true;
+        });
+    });
+});
