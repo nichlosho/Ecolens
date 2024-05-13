e92b3a02a18895960ac32222cbadb609a5e31ab6
Nichlos Ho <hon1@seattleu.edu>
2024-05-13 02:47:04 -0700

    generate js and sourceMaps into dist folder and remove commited js files

--- src/helper/collectionFilter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createCaseInsensitiveFilter = void 0;
-// Helper function to generate case-insensitive filter for documents
-function createCaseInsensitiveFilter(filter, propertyName, propertyValue) {
-    filter[propertyName] = { $regex: new RegExp(propertyValue, 'i') };
-    return filter;
-}
-exports.createCaseInsensitiveFilter = createCaseInsensitiveFilter;
