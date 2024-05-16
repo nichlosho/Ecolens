ec36a0c1a31b39cf5e709e008e56786cdc0c47e5
Nichlos Ho <hon1@seattleu.edu>
2024-05-16 11:23:26 -0700

    update baseUrl

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://ecolens.azurewebsites.net:',
+    baseUrl: 'https://ecolens.onrender.com',
     mongoDBConnection:
         'mongodb+srv://newuser:jmw2LNYyIMv7gCBR@ecolens.dkykdv2.mongodb.net/Ecolens',
-    backendPort: '3000',
+    backendPort: '',
 };
