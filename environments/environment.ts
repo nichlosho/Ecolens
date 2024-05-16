8cdb3f82d3a431a16fa76e95e5f77d97b95805fd
Nichlos Ho <57884925+nichlosho@users.noreply.github.com>
2024-05-16 13:53:27 -0700

    Delete environments directory

--- environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    baseUrl: 'http://localhost:',
-    mongoDBConnection:
-        'mongodb+srv://newuser:jmw2LNYyIMv7gCBR@ecolens.dkykdv2.mongodb.net/Ecolens',
-    backendPort: '3000',
-};
