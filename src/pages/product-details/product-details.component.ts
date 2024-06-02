a627fe8b803fa36ab9653eeef9bcb2e93fc4a54e
benwakaba <101528329+benwakaba@users.noreply.github.com>
2024-06-02 10:38:31 -0700

    updated product info UI

--- src/pages/product-details/product-details.component.ts
+++ src/pages/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from '../../interfaces/IProduct';
 import { ProductService } from '../../service/product.service';
@@ -16,6 +16,10 @@ export class ProductDetailsComponent implements OnInit {
         private route: ActivatedRoute
     ) {}
 
+    @Output() cartUpdated = new EventEmitter<{
+      product: Promise<IProduct>
+    }>();
+
     ngOnInit(): void {
         this.route.queryParams.subscribe(async (params) => {
             const id = params['id'];
@@ -28,5 +32,12 @@ export class ProductDetailsComponent implements OnInit {
                 console.error('Failed to fetch product details', error);
             }
         });
+    }
+
+    onCartUpdated(event) {
+      const id = event.target.getAttribute('id');
+      this.cartUpdated.emit({
+        product: this.productService.getProductById(id)
+      });
     }
 }
