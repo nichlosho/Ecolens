c49a3b248f86f203e73f9cb2e2ac80ce4b145a1f
Rakesh Utekar <48244158+rakeshutekar@users.noreply.github.com>
2024-06-03 17:57:15 -0700

    Checkout page
    Added Stepper to checkout page

--- src/pages/checkout/checkout.component.ts
+++ src/pages/checkout/checkout.component.ts
@@ -1,30 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'app-checkout',
-    templateUrl: './checkout.component.html',
-    styleUrl: './checkout.component.scss',
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
-    constructor() {}
+  addressFormGroup: FormGroup;
+  paymentFormGroup: FormGroup;
 
-    ngOnInit(): void {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        const forms = document.getElementsByClassName('needs-validation');
+  constructor(private _formBuilder: FormBuilder) {}
 
-        // Loop over them and prevent submission
-        Array.prototype.filter.call(forms, function (form: HTMLFormElement) {
-            form.addEventListener(
-                'submit',
-                function (event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                    form.classList.add('was-validated');
-                },
-                false
-            );
-        });
-    }
+  ngOnInit(): void {
+    this.addressFormGroup = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+      address2: [''],
+      country: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required]
+    });
+    this.paymentFormGroup = this._formBuilder.group({
+      ccName: ['', Validators.required],
+      ccNumber: ['', Validators.required],
+      ccExpiration: ['', Validators.required],
+      ccCvv: ['', Validators.required]
+    });
+  }
 }
